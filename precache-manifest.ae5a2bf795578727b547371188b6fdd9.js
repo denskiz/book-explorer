f7dffadfa45bd1367c203a6cf3c5c09643bcd87d
dennis <dennisent@protonmail.com>
2020-09-12 19:04:41 +1000

    Updates

---
+++ precache-manifest.ae5a2bf795578727b547371188b6fdd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8ce140730b7105917d31e6907bc0ac",
+    "url": "/book-explorer/index.html"
+  },
+  {
+    "revision": "b0c9138648dd5d48b937",
+    "url": "/book-explorer/static/css/main.225c9ddb.chunk.css"
+  },
+  {
+    "revision": "83bc21f1d22d964d3f27",
+    "url": "/book-explorer/static/js/2.724ef497.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-explorer/static/js/2.724ef497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c9138648dd5d48b937",
+    "url": "/book-explorer/static/js/main.e09ecd00.chunk.js"
+  },
+  {
+    "revision": "6b3684d295f524636b51",
+    "url": "/book-explorer/static/js/runtime-main.7f2906a9.js"
+  }
+]);
